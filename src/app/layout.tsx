dee636b52fb97ba445bef4f01338d2134df2f676
oddvar zakarias kristiansen <oddvarzk@gmail.com>
2025-04-06 13:18:46 +0200

    Speed Vercel Analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header></Header>
+        <Header />
         {children}
-        <Footer></Footer>
+        <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
