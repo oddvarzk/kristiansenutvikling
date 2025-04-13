17fbad06401693cbea9e989261b61daa34735d6e
Oddvar Kristiansen <oddvarzk@gmail.com>
2025-04-14 00:46:38 +0200

    New color

--- src/app/personvern/page.tsx
+++ src/app/personvern/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function PrivacyPolicy() {
   return (
     <div className="container mx-auto px-4 py-12 max-w-4xl">
-      <h1 className="text-3xl font-bold mb-6 text-gray-800">
+      <h1 className="text-3xl font-bold mb-6 text-cyan-700">
         Personvernerklæring
       </h1>
 
