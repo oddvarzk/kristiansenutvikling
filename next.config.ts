85601bc3e60449d44e204ced800a295c4eb7530b
Oddvar Kristiansen <oddvarzk@gmail.com>
2025-05-12 01:48:34 +0200

    Robots file added and connected

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* your other config options here */
+
+  async rewrites() {
+    return [
+      {
+        source: "/robots.txt",
+        destination: "/api/robots.txt",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
