97bdb218e33f2dae08583ead54e7bb2c56b99abe
Oddvar Kristiansen <oddvarzk@gmail.com>
2025-05-14 19:32:03 +0200

    Robots file fixed

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,35 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* your other config options here */
 
+  async redirects() {
+    return [
+      {
+        // Redirect all requests from kristiansenutvikling.com → kristiansenutvikling.no
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "kristiansenutvikling.com",
+          },
+        ],
+        destination: "https://kristiansenutvikling.no/:path*",
+        permanent: true,
+      },
+      {
+        // Redirect all requests from www.kristiansenutvikling.com → kristiansenutvikling.no
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.kristiansenutvikling.com",
+          },
+        ],
+        destination: "https://kristiansenutvikling.no/:path*",
+        permanent: true,
+      },
+    ];
+  },
+
   async rewrites() {
     return [
       {
