8990baa4e2a1780f48560d524594c6899c84f482
Oddvar Zakarias Kristiansen <122876488+oddvarzk@users.noreply.github.com>
2025-07-24 00:41:39 +0200

    eslint fix unused code

--- src/app/(pages)/kontakt/KontaktPage.tsx
+++ src/app/(pages)/kontakt/KontaktPage.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import KontaktForm from "./components/KontaktForm";
 import BackToTop from "../../components/BackToTop";
-import point from "../../../../public/images/point.svg";
 import { useTranslations } from "@/app/hooks/useTranslations";
 import { Mail, Phone, MapPin } from "lucide-react";
 
